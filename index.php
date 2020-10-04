5e7d1e258282effcca368edced8d02966f243555
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-05 00:09:14 +0300

    Finish up login

--- index.php
+++ index.php
@@ -1,19 +1,37 @@
 <?php
- require_once "configuration.php";
+ //require_once "configuration.php";
  ?>
-
+<!DOCTYPE html>
 <html>
 <head>
     <title>Sign in - Skyfallen</title>
     <link type="text/css" rel="stylesheet" href="css/main.css">
+    <script type="text/javascript" rel="script" src="js/main.js"></script>
+    <script type="text/javascript" rel="script" src="js/jquery.min.js"></script>
 </head>
 
 <body>
-<div class="centered">
-    <form method="post">
-        <input type="text" name="username" id="uninput" placeholder="Username/E-Mail" class="input-top">
-        <input type="password" name="password" id="pwinput" placeholder="Password" class="input-bottom">
+    <form method="post" class="centered">
+        <div class="container centered text-center" id="outsider">
+            <div id="middle">
+            <div class="field text-center">
+                <img src="https://theskyfallen.company/wp-content/uploads/2020/07/IMG_0183.png" style="height: 100px;">
+            </div>
+            <div class="field text-center">
+                <h3>Login to Skyfallen App Center</h3>
+            </div>
+            <div class="field">
+                <input type="text" required autocomplete="off" id="username" name="username" style="border-radius: 10px 10px 0px 0px;">
+                <label for="username" title="Please enter your Username or E-Mail" data-title="Username or E-Mail"></label>
+            </div>
+
+            <div class="field">
+                <input type="password" required autocomplete="off" id="password" name="password" style="border-radius: 0px 0px 10px 10px; border-top: none;">
+                <label for="password" title="Please enter your password." data-title="Password"></label>
+                <button type="submit"><img src=ŞM></button>
+            </div>
+            </div>
+        </div>
     </form>
-</div>
 </body>
 </html>
