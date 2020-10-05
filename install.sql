4ae2ef0e2d7ef6a757db398ff89c8345c03e3e07
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-05 13:44:26 +0300

    Add SQL Install

---
+++ install.sql
@@ -0,0 +1,23 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `verified` varchar(255) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+  CREATE TABLE `apps` (
+    `appid` varchar(255) NOT NULL,
+    `appname` varchar(255) NOT NULL,
+    `appsecret` varchar(255) NOT NULL,
+    `creator` varchar(255) NOT NULL,
+    `ispublic` varchar(255) NOT NULL,
+    `verified` varchar(255) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+  ALTER TABLE `apps`
+  ADD UNIQUE KEY `appid` (`appid`);
