382bbf6897c5f5fd2f193d42ee0c05107ae33c39
Yiğit Kerem Oktay <yigitkerem@theskyfallen.com>
2020-10-05 10:32:35 +0300

    Finish login animations

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 <img src="https://theskyfallen.company/wp-content/uploads/2020/07/IMG_0183.png" style="height: 100px;">
             </div>
             <div class="field text-center">
-                <h3>Login to Skyfallen App Center</h3>
+                <h3>Sign to Skyfallen App Center</h3>
             </div>
             <div class="field">
                 <input type="text" required autocomplete="off" id="username" name="username" style="border-radius: 10px 10px 0px 0px;">
@@ -28,7 +28,9 @@
             <div class="field">
                 <input type="password" required autocomplete="off" id="password" name="password" style="border-radius: 0px 0px 10px 10px; border-top: none;">
                 <label for="password" title="Please enter your password." data-title="Password"></label>
-                <button type="submit"><img src=ŞM></button>
+            </div>
+            <div class="field-button">
+                <button type="button" style="border: none; background: transparent;"><img src="img/RightArrow.png" style="height: 30px;"></button>
             </div>
             </div>
         </div>
